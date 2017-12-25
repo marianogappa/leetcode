8200a1616fcd4097f1599b47d7073104b4f7ff10
Mariano Gappa <spinetta@gmail.com>
2017-12-25 19:06:33 +1300

    https://leetcode.com/problems/find-peak-element

--- find-peak-element.go
+++ find-peak-element.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+func bs(nums []int, min, max int) int {
+	if min == max {
+		return min
+	}
+	var (
+		m     = (min + max) / 2
+		left  = m - 1
+		right = m + 1
+	)
+	if left < min {
+		left = min
+	}
+	if right > max {
+		right = max
+	}
+	switch {
+	case nums[left] < nums[m] && nums[m] < nums[right]:
+		return bs(nums, m, max)
+	case nums[left] > nums[m] && nums[m] > nums[right]:
+		return bs(nums, min, m)
+	case nums[left] > nums[m] && nums[m] < nums[right]:
+		return bs(nums, m, max)
+	default:
+		return m
+	}
+}
+
 func findPeakElement(nums []int) int {
 	if len(nums) == 0 {
 		return 0 // to avoid panic
@@ -15,17 +42,7 @@ func findPeakElement(nums []int) int {
 	if nums[len(nums)-2] < nums[len(nums)-1] {
 		return len(nums) - 1
 	}
-	j := len(nums) - 2
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
-			return i
-		}
-		if nums[j] > nums[j-1] && nums[j] > nums[j+1] {
-			return j
-		}
-		j--
-	}
-	return 0 // to avoid compile error
+	return bs(nums, 0, len(nums)-1)
 }
 
 func main() {
@@ -38,6 +55,7 @@ func main() {
 		{i: []int{2, 1}, e: 0},
 		{i: []int{1, 2}, e: 1},
 		{i: []int{1, 2, 3, 1}, e: 2},
+		{i: []int{1, 3, 2, 1}, e: 1},
 		{i: []int{1, 2, 3, 5, 6, 7, 8, 9, 10, 11}, e: 9},
 	}
 	for _, t := range ts {
