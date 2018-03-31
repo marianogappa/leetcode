2bbdccd9c29a3ac815e163213cd1134ed3339df0
Mariano Gappa <spinetta@gmail.com>
2018-03-31 20:20:06 +1300

    https://leetcode.com/problems/meeting-rooms-ii

--- meeting-rooms-ii.go
+++ meeting-rooms-ii.go
@@ -10,7 +10,48 @@ type Interval struct {
 	End   int
 }
 
-func qsort(is []int) {
+// Time: O(n*logn) Space: O(n)
+// If you could traverse the numbers in order (e.g. by sorting),
+// every meeting time is a +1 and every meeting end is a -1 in
+// "minMeetingRooms". Keep track of the global maximum.
+func minMeetingRooms(intervals []Interval) int {
+	var (
+		cur, max, i, j int
+		plus, minus    = make([]int, len(intervals)), make([]int, len(intervals))
+	)
+
+	// Time: O(n) Space: O(n)
+	// Copy all ".Start"s on a "plus" slice and all ".End"s on a "minus" slice
+	for k := range intervals {
+		plus[k] = intervals[k].Start
+		minus[k] = intervals[k].End
+	}
+
+	// Time: O(n*logn) Space: O(1)
+	// Sort both slices
+	quicksort(plus)
+	quicksort(minus)
+
+	// Time: O(n) Space: O(1)
+	// Traverse pluses and minuses in order: each ".Start" is a +1 and each
+	// ".End" is a -1
+	for i < len(plus) && j < len(minus) {
+		if plus[i] < minus[j] {
+			cur++
+			if cur > max { // Whenever you +1, update max if necessary
+				max = cur
+			}
+			i++
+		} else {
+			cur--
+			j++
+		}
+	}
+	return max
+}
+
+// This is vanilla quicksort; can be replaced with sorts.Ints
+func quicksort(is []int) {
 	if len(is) <= 1 {
 		return
 	}
@@ -27,34 +68,8 @@ func qsort(is []int) {
 		}
 	}
 	is[r], is[l] = is[l], is[r]
-	qsort(is[:l])
-	qsort(is[l+1:])
-}
-
-func minMeetingRooms(intervals []Interval) int {
-	var (
-		cur, max, i, j int
-		plus, minus    = make([]int, len(intervals)), make([]int, len(intervals))
-	)
-	for k := range intervals {
-		plus[k] = intervals[k].Start
-		minus[k] = intervals[k].End
-	}
-	qsort(plus)
-	qsort(minus)
-	for i < len(plus) && j < len(minus) {
-		if plus[i] < minus[j] {
-			cur++
-			if cur > max {
-				max = cur
-			}
-			i++
-		} else {
-			cur--
-			j++
-		}
-	}
-	return max
+	quicksort(is[:l])
+	quicksort(is[l+1:])
 }
 
 func main() {
