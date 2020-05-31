0cca7a9cef5d5e771b1a5bb9a862c4bc58abedb7
Mariano Gappa <spinetta@gmail.com>
2020-05-31 20:42:00 +0100

    https://leetcode.com/problems/container-with-most-water

--- 2020/container-with-most-water/main.go
+++ 2020/container-with-most-water/main.go
@@ -2,39 +2,27 @@ package main
 
 import "fmt"
 
-// Strategy is to start from both edges of the map and narrow
-// that range until all interesting areas are covered, linearly
-// but without losing any interesting areas.
-//
-// The trick to keep it linear is to move the "cursors" one at a
-// time, and always moving the smallest first. Do the exercise
-// manually on a whiteboard to check, but this way no interesting
-// areas are lost. Otherwise, the time complexity is quadratic.
-//
 // Time: O(n)
 // Space: O(1)
 func maxArea(height []int) int {
 	var (
-		start = 0
-		end   = len(height) - 1
-		mx    int
+		l  = 0
+		r  = len(height) - 1
+		mx = 0
 	)
-	for start < end {
-		mx = max(mx, min(height[start], height[end])*(end-start))
-		if height[start] < height[end] {
-			start++
+	for l < r {
+		mx = max(mx, min(height[l], height[r])*(r-l))
+		// This is the key: as long as we move one
+		// pointer at a time, and we move the one with
+		// the lower height, we won't lose any
+		// interesting areas.
+		if height[l] < height[r] {
+			l++
 		} else {
-			end--
+			r--
 		}
 	}
 	return mx
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 func min(a, b int) int {
@@ -43,7 +31,27 @@ func min(a, b int) int {
 	}
 	return b
 }
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func main() {
-	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}) == 49)
+	ts := []struct {
+		input    []int
+		expected int
+	}{
+		{
+			input:    []int{1, 8, 6, 2, 5, 4, 8, 3, 7},
+			expected: 49,
+		},
+	}
+	for _, tc := range ts {
+		actual := maxArea(tc.input)
+		if tc.expected != actual {
+			fmt.Printf("For %v expected %v but got %v\n", tc.input, tc.expected, actual)
+		}
+	}
 }
