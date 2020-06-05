cb5dbcf7187e3ed39af0d18f87df6da8489245be
Mariano Gappa <spinetta@gmail.com>
2020-06-05 18:17:39 +0100

    https://leetcode.com/problems/unique-paths

--- 2020/unique-paths/main.go
+++ 2020/unique-paths/main.go
@@ -2,49 +2,39 @@ package main
 
 import "fmt"
 
-// This is basic DP with memo.
-//
-// The possible paths at a given position is equal to the
-// sum of possible paths from the neighbouring down and
-// right cells, and that number is the same for every
-// branch that leads there, so we can store it in a map.
-//
-// Thus every position's possible paths will eventually be
-// on the map, and we don't need to calculate them more
-// than once. This is why is linear both in space and time.
-//
-// Optimisation: when we reach the right or bottom corner,
-// there's only one possible solution from there, so we
-// don't need to continue: the result is 1 from there.
-//
-// Time: O(m*n) because of the memoisation
-// Space: O(m*n) because every grid cell will be on the map
+type xy struct {
+	x, y int
+}
+
+// Time: O(m*n)
+// Space: O(m*n)
 func uniquePaths(m int, n int) int {
 	if m == 0 || n == 0 {
 		return 0
 	}
+	// -1 is to make it zero-based
 	return doUniquePaths(m-1, n-1, map[xy]int{})
 }
 
-type xy struct{ x, y int }
-
 func doUniquePaths(m int, n int, memo map[xy]int) int {
+	if m < 0 || n < 0 {
+		return 0
+	}
 	if m == 0 && n == 0 {
 		return 1
 	}
-	if _, ok := memo[xy{m, n}]; !ok {
-		if m > 0 && n > 0 {
-			memo[xy{m, n}] = doUniquePaths(m-1, n, memo) + doUniquePaths(m, n-1, memo)
-		} else {
-			memo[xy{m, n}] = 1
-		}
+	pos := xy{m, n}
+	if v, ok := memo[pos]; ok {
+		return v
 	}
-	return memo[xy{m, n}]
+	memo[pos] = doUniquePaths(m-1, n, memo) + doUniquePaths(m, n-1, memo)
+	return memo[pos]
 }
 
 func main() {
 	ts := []struct {
-		m, n     int
+		m        int
+		n        int
 		expected int
 	}{
 		{
@@ -61,7 +51,7 @@ func main() {
 	for _, tc := range ts {
 		actual := uniquePaths(tc.m, tc.n)
 		if tc.expected != actual {
-			fmt.Printf("For (%v, %v) expected %v but got %v\n", tc.m, tc.n, tc.expected, actual)
+			fmt.Printf("For (%v,%v) expected %v but got %v\n", tc.m, tc.n, tc.expected, actual)
 		}
 	}
 }
