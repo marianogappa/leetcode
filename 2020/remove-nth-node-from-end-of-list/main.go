5f225ea922046cefd560dd54687f2fe2fd2d4f53
Mariano Gappa <spinetta@gmail.com>
2020-06-12 20:54:34 +0100

    https://leetcode.com/problems/remove-nth-node-from-end-of-list

--- 2020/remove-nth-node-from-end-of-list/main.go
+++ 2020/remove-nth-node-from-end-of-list/main.go
@@ -7,65 +7,54 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// Convenient method for printing single linked lists
 func (n *ListNode) String() string {
 	if n == nil {
-		return "nil"
+		return ""
 	}
-	return fmt.Sprintf("(%v)->%v", n.Val, n.Next)
+	return fmt.Sprintf("%v -> %v", n.Val, n.Next)
 }
 
-// In order to solve this in one pass, we must maintain a queue
-// of n+1 items. This is because we need to stand on the node that
-// comes right before the one to be chopped off.
-//
-// The simplest way I could think of maintaining this queue is with
-// a "static" array of n+1 length and a rotating index to the "current"
-// element in the queue. When we finish iterating the list, the target
-// node (the one before the victim) will be the oldest pushed in the queue.
-//
-// There's an edge case: if the length of the list equals n, we cannot
-// stand before the victim. In this case, just return head.Next.
-//
-// Time: O(n), also one pass
-// Space: O(n), where n is the n from arguments + 1
+// Time: O(n)
+// Space: O(1)
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	if head == nil {
 		return nil
 	}
-	var (
-		queue       = make([]*ListNode, n+1)
-		cur         = head
-		i, totalLen int
-	)
-	for cur != nil {
-		totalLen++
-		queue[i] = cur
-		i++
-		if i >= len(queue) {
-			i = 0
-		}
-		cur = cur.Next
+	ahead := head
+	beforeTarget := head
+
+	// By having a pointer n steps ahead of
+	// _beforeTarget_, if we move both at
+	// the same time until we reach the end,
+	// _beforeTarget_ will be in position to
+	// remove the target node.
+	for i := 0; i < n; i++ {
+		ahead = ahead.Next
 	}
-	// special case for n == totalLen
-	if totalLen == n {
+
+	// Edge case: removing the head.
+	if ahead == nil {
 		return head.Next
 	}
 
-	queue[i].Next = queue[i].Next.Next
+	// Move both pointers at the same time
+	// until ahead is at the tail.
+	for ahead.Next != nil {
+		ahead = ahead.Next
+		beforeTarget = beforeTarget.Next
+	}
+
+	// Remove target node.
+	beforeTarget.Next = beforeTarget.Next.Next
+
 	return head
 }
 
 func main() {
-	l := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
-	fmt.Println(removeNthFromEnd(l, 1).String() == "(1)->(2)->(3)->nil")
-
-	l = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
-	fmt.Println(removeNthFromEnd(l, 2).String() == "(1)->(2)->(4)->nil")
-
-	l = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
-	fmt.Println(removeNthFromEnd(l, 3).String() == "(1)->(3)->(4)->nil")
-
-	l = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
-	fmt.Println(removeNthFromEnd(l, 4).String() == "(2)->(3)->(4)->nil")
+	fmt.Println(removeNthFromEnd(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 1))
+	fmt.Println(removeNthFromEnd(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 2))
+	fmt.Println(removeNthFromEnd(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 3))
+	fmt.Println(removeNthFromEnd(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 4))
+	fmt.Println(removeNthFromEnd(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 5))
+	fmt.Println(removeNthFromEnd(&ListNode{1, nil}, 1))
 }
