246f21f20b89a40c872230ed80f64c082c35c1ff
Mariano Gappa <spinetta@gmail.com>
2020-07-16 18:02:00 +0100

    https://leetcode.com/problems/word-break

--- 2020/word-break/main.go
+++ 2020/word-break/main.go
@@ -4,84 +4,51 @@ import (
 	"fmt"
 )
 
-// A Trie is necessary because it's important that going
-// through the string whilst matching to the dictionary
-// is a linear algorithm. Otherwise we'd have to keep
-// indices and loop through words to match.
-type trie struct {
-	nodes map[byte]*trie
-	stop  bool
-}
-
-func (t *trie) put(s string) {
-	if len(s) == 0 {
-		return
-	}
-	if t.nodes == nil {
-		t.nodes = make(map[byte]*trie)
-	}
-	if _, ok := t.nodes[s[0]]; !ok {
-		t.nodes[s[0]] = &trie{make(map[byte]*trie), len(s) == 1}
-	}
-	t.nodes[s[0]].stop = t.nodes[s[0]].stop || len(s) == 1
-	t.nodes[s[0]].put(s[1:])
-}
-
-func (t *trie) moveTo(b byte) *trie {
-	if t.nodes == nil || t.nodes[b] == nil {
-		return nil
-	}
-	return t.nodes[b]
-}
-
-// Time: O(n) because of memo
-// Space: O(n+m) length of string (memo and stack) + length of wordDict's letters (stored in trie)
+// Time: O(s^3) two for-loops plus a string comparison of length s in the worst case
+// Space: O(w*w'+s) hashmap of words and bool array of size len(s)
 func wordBreak(s string, wordDict []string) bool {
-	// First construct a Trie with the dictionary to achieve efficient matching.
-	t := &trie{}
+	m := make(map[string]bool, len(wordDict))
 	for _, word := range wordDict {
-		t.put(word)
+		m[word] = true
 	}
 
-	// While solving the matching, keep a memo that answers if a solution can
-	// be reached from a given position on the string 's'.
-	return doWordBreak(s, 0, t, map[int]bool{})
+	// dp[i] means "is there a valid segmentation of _s_
+	// up to the ith index?"
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			// If there's a valid segmentation up to _j_
+			// and s[j:i] is a valid word, then there's a
+			// valid segmentation up to _i_.
+			if dp[j] && m[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
 }
 
-func doWordBreak(s string, i int, t *trie, memo map[int]bool) bool {
-	if i >= len(s) {
-		return true
-	}
-	if v, ok := memo[i]; ok {
-		return v
-	}
+// Time: O(s*w*max(w',s))
+// Space: O(s)
+func anotherWordBreak(s string, wordDict []string) bool {
+	// dp[i] means "is there a valid segmentation of _s_
+	// up to the ith index?"
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
 
-	// Starting at the roots of the Trie, go through the Trie as
-	// we go through the string, trying to match to dictionary
-	// words.
-	cursor := t
-	for j := i; j < len(s); j++ {
-		cursor = cursor.moveTo(s[j])
-		// If there was no match on the Trie, then this string
-		// cannot be constructed from the dictionary words.
-		if cursor == nil {
-			memo[i] = false
-			return false
-		}
-		// If there was a match, and the Trie says a word ends
-		// on this character, then check recursively if there
-		// is a solution from the remaining substring.
-		//
-		// IMPORTANT! If there's no solution from here, there
-		// still might be a solution for a larger dictionary
-		// word than the one we just matched, so keep going!
-		if cursor.stop && doWordBreak(s, j+1, t, memo) {
-			memo[i] = true
-			return true
+	for i := 1; i <= len(s); i++ {
+		for _, word := range wordDict {
+			// If there's a word that ends at index _i_ of _s_, and starts
+			// at an already valid index, then _dp[i]_ is also valid.
+			if len(word) <= i && dp[i-len(word)] && s[i-len(word):i] == word {
+				dp[i] = true
+			}
 		}
 	}
-	memo[i] = false
-	return false
+	return dp[len(s)]
 }
 
 func main() {
@@ -112,7 +79,7 @@ func main() {
 		},
 	}
 	for _, tc := range ts {
-		actual := wordBreak(tc.s, tc.wordDict)
+		actual := anotherWordBreak(tc.s, tc.wordDict)
 		if tc.expected != actual {
 			fmt.Printf("For (%v,%v) expected %v but got %v\n", tc.s, tc.wordDict, tc.expected, actual)
 		}
