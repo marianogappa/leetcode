2bf72b3206032a3234f916e676c3aea5e60fbcff
Mariano Gappa <spinetta@gmail.com>
2022-09-20 10:12:30 +0100

    https://leetcode.com/problems/simplify-path

---
+++ 2022/simplify-path/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = parse_path(path)
+        return "/" + "/".join(stack)
+
+def parse_path(path: str) -> list[str]:
+    stack = []
+    for part in path.split('/'):
+        if part == "" or part == ".":
+            continue
+        elif part == ".." and len(stack) > 0:
+            stack.pop()
+        elif part != "..":
+            stack.append(part)
+    
+    return stack
+
+print(Solution().simplifyPath("/home/"), "== /home")
+print(Solution().simplifyPath("/../"), "== /")
+print(Solution().simplifyPath("/home//foo/"), "== /home/foo")
